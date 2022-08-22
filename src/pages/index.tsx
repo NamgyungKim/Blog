56bc7956d366a9e6945a21faee1d3cf4709cb3d0
NamgyungKim <namgyung.kim@gmail.com>
2022-08-22 22:34:38 +0900

    feat: getsby-plugin-image 라이브러리로 image 띄우기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,9 @@ import GlobalStyle from 'components/Common/GlobalStyle'
 import Footer from 'components/Common/Footer'
 import Introduction from 'components/Main/Introduction'
 import PostList from 'components/Main/PostList'
+// import CategoryList from 'components/Main/CategoryList'
 import { graphql } from 'gatsby'
+import { IGatsbyImageData } from 'gatsby-plugin-image'
 import { PostListItemType } from 'types/PostItem.types'
 
 type IndexPageProps = {
@@ -12,8 +14,19 @@ type IndexPageProps = {
     allMarkdownRemark: {
       edges: PostListItemType[]
     }
+    file: {
+      childImageSharp: {
+        gatsbyImageData: IGatsbyImageData
+      }
+    }
   }
 }
+
+// const CATEGORY_LIST = {
+//   All: 5,
+//   Web: 3,
+//   Mobile: 2,
+// }
 
 const Container = styled.div`
   display: flex;
@@ -24,12 +37,16 @@ const Container = styled.div`
 const IndexPage: FunctionComponent<IndexPageProps> = function ({
   data: {
     allMarkdownRemark: { edges },
+    file: {
+      childImageSharp: { gatsbyImageData },
+    },
   },
 }) {
   return (
     <Container>
       <GlobalStyle />
-      <Introduction />
+      <Introduction profileImage={gatsbyImageData} />
+      {/* <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} /> */}
       <PostList posts={edges} />
       <Footer />
     </Container>
@@ -52,10 +69,17 @@ export const getPostList = graphql`
             date(formatString: "YYYY.MM.DD.")
             categories
             thumbnail {
-              publicURL
+              childImageSharp {
+                gatsbyImageData(width: 768, height: 400)
+              }
             }
           }
         }
+      }
+    }
+    file(name: { eq: "profile-image" }) {
+      childImageSharp {
+        gatsbyImageData(width: 120, height: 120)
       }
     }
   }
