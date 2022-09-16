a3725c578fa3a7352a494dea4a836f750c046045
NamgyungKim <namgyung.kim@gmail.com>
2022-09-16 20:00:09 +0900

    style: reset 파일 추가

--- src/components/Common/GlobalStyle.tsx
+++ src/components/Common/GlobalStyle.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from 'react'
 import { Global, css } from '@emotion/react'
+import '../../styles/reset.css'
 
 const defaultStyle = css`
   @import url('https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@400;700;800&display=swap');
