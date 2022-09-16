1aeebb853c13a43f0728a1644f6c15fba237f057
NamgyungKim <namgyung.kim@gmail.com>
2022-09-16 23:18:22 +0900

    feat: footer 링크 추가

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -1,23 +1,41 @@
 import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
-
-const FooterWrapper = styled.div`
-  display: grid;
-  place-items: center;
-  margin-top: auto;
-  padding: 50px 0;
-  font-size: 15px;
-  text-align: center;
-  line-height: 1.5;
-`
+import { BsGithub } from 'react-icons/bs'
+import { FiMail } from 'react-icons/fi'
+import { RiCodeSSlashFill } from 'react-icons/ri'
+import { color, text } from '../../styles/theme'
 
 const Footer: FunctionComponent = function () {
   return (
     <FooterWrapper>
       Thank You for Visiting My Blog, Have a Good Day 😆
-      <br />© 2021 Developer Hyun, Powered By Gatsby.
+      <br />© 2022 Nadia’s Blog · Made with Gatsby
+      <LinkIcon>
+        <a href="https://github.com/NamgyungKim" target="_blank">
+          <BsGithub size={30} color={color.$gray500} />
+        </a>
+        <a href="mailto:namgyung.kim@gmail.com" target="_blank">
+          <FiMail size={30} color={color.$gray500} />
+        </a>
+        <a href="https://github.com/NamgyungKim/Blog" target="_blank">
+          <RiCodeSSlashFill size={30} color={color.$gray500} />
+        </a>
+      </LinkIcon>
     </FooterWrapper>
   )
 }
 
 export default Footer
+
+const FooterWrapper = styled.div`
+  ${text.$body1}
+  color: ${color.$gray500};
+  text-align: center;
+  cursor: default;
+`
+const LinkIcon = styled.div`
+  margin-top: 20px;
+  a {
+    margin: 0 4px;
+  }
+`
