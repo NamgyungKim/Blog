f87ff25bc56f895bbc8e79d6817008319054e838
NamgyungKim <namgyung.kim@gmail.com>
2022-09-18 23:52:26 +0900

    fix: storybook에서 Link 컴포넌트 오류나는 문제 해결

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
 import '../src/styles/reset.css'
+import { action } from '@storybook/addon-actions'
+
+global.___loader = {
+  enqueue: () => {},
+  hovering: () => {},
+}
+global.__BASE_PATH__ = '/'
+
+window.___navigate = pathname => {
+  action('NavigateTo:')(pathname)
+}
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
