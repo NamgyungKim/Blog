2e605986e7b07bf122d49edea8c1cf680e6d7f4a
NamgyungKim <namgyung.kim@gmail.com>
2022-09-22 18:02:10 +0900

    feat: about페이지 추가

---
+++ src/pages/about.tsx
@@ -0,0 +1,8 @@
+import PageLayout from 'components/Common/PageLayout'
+import React from 'react'
+
+const AboutPage = () => {
+  return <PageLayout>공사중... 🚧</PageLayout>
+}
+
+export default AboutPage
