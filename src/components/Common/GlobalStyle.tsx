3500b25b0ca2b17756026ae97e072c67e34cbd4e
NamgyungKim <namgyung.kim@gmail.com>
2022-10-07 09:39:01 +0900

    remove: font 파일제거

--- src/components/Common/GlobalStyle.tsx
+++ src/components/Common/GlobalStyle.tsx
@@ -4,65 +4,6 @@ import '../../styles/reset.css'
 import { color } from '../../styles/theme'
 
 const defaultStyle = css`
-  // NotoSansKR
-  /* @font-face {
-    font-family: 'NotoSansKR';
-    font-style: normal;
-    font-weight: normal;
-    font-display: swap;
-    src: url('../font/NotoSansKR-Regular.otf');
-  }
-  @font-face {
-    font-family: 'NotoSansKR';
-    font-style: normal;
-    font-weight: bold;
-    font-display: swap;
-    src: url('../font/NotoSansKR-bold.otf');
-  }
-  @font-face {
-    font-family: 'NotoSansKR';
-    font-style: normal;
-    font-weight: lighter;
-    font-display: swap;
-    src: url('../font/NotoSansKR-Light.otf');
-  } */
-  // Roboto
-  /* @font-face {
-    font-family: 'Roboto';
-    font-style: normal;
-    font-weight: bold;
-    font-display: swap;
-    src: url('../font/Roboto-Bold.ttf');
-  }
-  @font-face {
-    font-family: 'Roboto';
-    font-style: normal;
-    font-weight: normal;
-    font-display: swap;
-    src: url('../font/Roboto-Regular.ttf');
-  }
-  @font-face {
-    font-family: 'Roboto';
-    font-style: normal;
-    font-weight: lighter;
-    font-display: swap;
-    src: url('../font/Roboto-Light.ttf');
-  }
-  @font-face {
-    font-family: 'Roboto';
-    font-style: italic;
-    font-weight: normal;
-    font-display: swap;
-    src: url('../font/Roboto-Light.ttf');
-  }
-  @font-face {
-    font-family: 'Roboto';
-    font-style: italic;
-    font-weight: lighter;
-    font-display: swap;
-    src: url('../font/Roboto-LightItalic.ttf');
-  } */
-
   * {
     padding: 0;
     margin: 0;
